b155630f3365237f5f6358eabe19013d999b6d58
foysal chowdhury <mamtawahid@gmail.com>
2020-09-15 22:50:06 -0400

    Homework1

---
+++ src/DataTypeExercise/Homework1.java
@@ -0,0 +1,57 @@
+package DataTypeExercise;
+
+public class Homework1 {
+
+    public static void main(String[] args) {
+
+        // 1.  Numbers of hours in a day
+
+        byte totalDayHours = 24;
+        System.out.println("There are "+ totalDayHours + " hours in a day.");
+
+        // 2. Maximum number of days in a year
+
+        short daysInAYear = 365;
+        System.out.println("There are maximum "+ daysInAYear + " days in a year.");
+
+        // 3. Total number of employees in an organization
+
+        short numOfEmployees = 1000;
+        System.out.println("Total number of employees in an organization is: "+ numOfEmployees);
+
+        // 4. Population in a country
+
+        int noOfPopulationInBd = 180000000;
+        System.out.println("Total number of population in Bangladesh: "+ noOfPopulationInBd );
+
+        // 5. Interest Rate
+
+        float interestRate = 1.1f;
+        System.out.println("Interest rate is: " + interestRate);
+
+        // 6. Balance in a Bank Account
+
+        double accountBalance = 500.50;
+        System.out.println("I have " +accountBalance + " balance in my bank account.");
+
+        // 7. Does the sun rise from the west?
+
+        boolean doesSunRisesWest = false;
+        System.out.println("It is " + doesSunRisesWest + " that sun rises from the west." );
+
+        // 8. Initials of my name
+
+        char firstInitial = 'M';
+        char secondInitial = 'W';
+        System.out.println("The initials of my name are: " + firstInitial +  secondInitial);
+
+        // 9. My full name
+
+        String myFullName = "Mamta Wahid";
+        System.out.println("My full name is "+ myFullName);
+
+
+
+
+    }
+}
